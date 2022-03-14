29849947aa86ffacbe0a1b7544f3d0fb5449d5d0
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2022-03-14 11:34:18 +0300

    Добавьте файлы проекта.

---
+++ Task17/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    class Account<T>
+    {
+        private T Num { get; set; }
+        private int Balance { get; set; }
+        private string Name { get; set; }
+
+        public string Print()
+        {
+            return $"Номер счёта: {Num} Баланс счёта: {Balance} ФИО владельца счёта: {Name}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счёта");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Укажите баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца счёта");
+            Name = Console.ReadLine();
+        }
+    }
+}
---
+++ Task17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    class Program
+    {
+        //Создать класс для моделирования счета в банке. Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.
+        //Класс должен быть объявлен как обобщенный. Универсальный параметр T должен определять тип номера счета.
+        //Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения.
+        //Создать  несколько экземпляров класса, параметризированного различными типам.
+        //Заполнить его поля и вывести информацию об экземпляре класса на печать.
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+            Console.WriteLine(account1.Print());
+            Account<string> account2 = new Account<string>();
+            account2.Input();
+            Console.WriteLine(account2.Print());
+            Console.ReadKey();
+        }
+    }
+}
